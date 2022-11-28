1bb69d5f01bf497109b3dfe3549e5bd9f04056b7
MariusAuerbacher <101823468+MariusAuerbacher@users.noreply.github.com>
2022-11-28 14:59:00 +0100

    HW-D6-JS

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstName = "Marius";
+let lastName = "Auerbacher";
+
+let fullName;
+fullName =firstName + " " + lastName;
+
+console.log("hello, my name is:", fullName);
+
+let myAge = 32;;
+let currentYear = 2022;
+let birthYear = currentYear - myAge;
+
+console.log("my birth year is:", birthYear);
